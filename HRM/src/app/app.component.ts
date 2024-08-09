e8b40f9407a8742c714db770a73eba83330d0334
Milindjadhav1507 <milindjadhav@MyrahConsultingServicesPriv.onmicrosoft.com>
2024-08-09 16:13:09 +0530

    Day 2
    Day 2

--- HRM/src/app/app.component.ts
+++ HRM/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
-import { NavbarComponent } from './account/navbar/navbar.component';
-import { FooterComponent } from './account/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,LoginComponent,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet,RouterLink,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
