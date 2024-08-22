caff54765eaf01c5855d531ab27d6e278d047814
Milindjadhav1507 <milindjadhav@MyrahConsultingServicesPriv.onmicrosoft.com>
2024-08-22 10:51:05 +0530

    22-08-2024
    web new design comes from UI-UX Desginer

--- HRM/src/app/app.routes.ts
+++ HRM/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { SidebarComponent } from './pages/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TestComponent } from './test/test.component';
 
 export const routes: Routes = [
     {path: 'account', loadChildren: () => import("./account/account-routing.module").then(m=>m.AccountRoutingModule)},
     {path: 'pages', loadChildren: () => import("./pages/pages-routing.module").then(m=>m.PagesRoutingModule)},
     {path:'dashboard',component:DashboardComponent},
     {path: '', component: SidebarComponent},
+    {path: 'test', component: TestComponent},
     
     
     
